c7dc116e2b2864300f9e77ad0876c3d57c822557
cassaram09 <cassaram09@gmail.com>
2017-09-21 22:17:36 -0400

    update db user name, create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, :defaults => { :format => 'json' } do
+    namespace :v1 do
+
+      resources :users, except: [:new, :create]
+      resources  :sessions, only: [:new, :create, :destroy]
+
+    end  
+  end
+
+  get '/login' => 'api/v1/sessions#new'
+  post '/login' => 'api/v1/sessions#create'
+  get '/logout' => 'api/v1/sessions#destroy'
+  post '/signup' => 'api/v1/users#create'
+  post '/password-reset' => 'api/v1/users#password'
+  
 end
