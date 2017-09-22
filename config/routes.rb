7b74431b16cbc2204b0fa0421e46016e25209b62
cassaram09 <cassaram09@gmail.com>
2017-09-21 22:46:03 -0400

    rename session to auth; add auth controller and auth to application controller; add JWT auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       resources :users, except: [:new, :create]
-      resources  :sessions, only: [:new, :create, :destroy]
+      resources  :auth, only: [:new, :create, :destroy]
 
     end  
   end
 
-  get '/login' => 'api/v1/sessions#new'
-  post '/login' => 'api/v1/sessions#create'
-  get '/logout' => 'api/v1/sessions#destroy'
+  get '/login' => 'api/v1/auth#new'
+  post '/login' => 'api/v1/auth#create'
+  get '/logout' => 'api/v1/auth#destroy'
   post '/signup' => 'api/v1/users#create'
   post '/password-reset' => 'api/v1/users#password'
   
