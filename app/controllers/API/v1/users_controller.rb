351d1f7631c6916a37684f29a43eaec9aa6c65f0
cassaram09 <cassaram09@gmail.com>
2017-09-21 22:48:06 -0400

    add users controller

---
+++ app/controllers/API/v1/users_controller.rb
@@ -0,0 +1,47 @@
+class API::v1::UsersController < ApplicationController
+  
+  skip_before_action :authenticate, only: :create
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      jwt = Auth.issue({user: @user.id})
+      render json: {jwt: jwt}
+    else 
+      render json: {error: "Sign up failed."}, status: 500
+    end
+  end
+
+  def reports
+
+  def update
+    @user = User.find_by(id: current_user.id)
+    if @user.update(user_params)
+      render json: @user
+    end
+  end
+
+  def get_current_user
+    @user = User.find_by(id: current_user.id)
+    render json: @user
+    return
+  end
+
+  def password_reset
+    @user = User.find_by(id: current_user.id)
+    if user_params[:password] == user_params[:password_confirmation] && @user == current_user
+      @user.update(user_params)
+      render json: {updated: true}
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  end
+
+end
