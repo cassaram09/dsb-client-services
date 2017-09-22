13f19d176e636a95a210304b196e49c7499d09e5
cassaram09 <cassaram09@gmail.com>
2017-09-21 23:23:14 -0400

    add auth and user resource files

---
+++ client/src/app/modules/user/userResource.js
@@ -0,0 +1,29 @@
+import Resource from 'r3-library';
+import request from 'superagent';
+
+import {API} from '../../utils/constants';
+import Store from '../../store/store';
+
+const url = '/users'
+
+const User = new Resource('user', url, API.headers)
+  .registerNewAction('/password-reset', 'changePassword', 'POST')
+  .registerNewAction('/current-user', 'getCurrentUser', 'GET', (state, action) => {
+    console.log('got USER', action.data)
+    return action.data
+    })
+  .addReducerAction('update', (state, action) => action.data)
+  .addResourceAction('/users', 'update', 'PATCH')
+  .updateReducerAction('uploadImage', (state, action) => action.data)
+  
+User.resourceActions.user_uploadImage = (file) => {
+  return new Promise((resolve, reject) => {
+    const req = request.post('/user-image').set('AUTHORIZATION', `Bearer ${sessionStorage.jwt}`)
+      req.attach('user[avatar]', file);
+      req.end(function(error, response){
+        resolve(response.body);
+      });
+  });
+}
+
+export default User;
