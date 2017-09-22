a1b2e2a72783e3a103bdaa93a0d030985d6924a7
cassaram09 <cassaram09@gmail.com>
2017-09-21 23:00:29 -0400

    rename and rework files structure of react client

---
+++ client/src/app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        I'm the app!
+      </div>
+    );
+  }
+}
+
+export default App;
